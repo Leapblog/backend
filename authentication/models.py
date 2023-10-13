6cd0b9c2adc62f08701d9d12a70881356763fce5
Aditi Kharel <aditikharel246@gmail.com>
2023-10-13 13:56:58 +0545

    Apply suggestions from code review
    Co-authored-by: Nishant Sapkota <60746512+thenishantsapkota@users.noreply.github.com>

--- authentication/models.py
+++ authentication/models.py
@@ -37,10 +37,8 @@ class Profile(models.Model):
         return self.user.username
 
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
-
-
-post_save.connect(create_profile, sender=User)
