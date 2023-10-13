0e40a3a613ec2c3ec237b0f645e3299809724f5a
Nishant Sapkota <snishant306@gmail.com>
2023-10-13 18:40:58 +0545

    chore: register profile in django admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,52 +1,71 @@
-# Register your models here.
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
+from .models import User, BlackListedToken, Profile
 
-from .models import BlackListedToken, Profile, User
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "display_image",
+        "bio",
+        "college",
+        "batch",
+        "website_link",
+        "linkedin_link",
+    )
+    search_fields = ("user__username", "user__email", "college", "batch")
+    list_filter = ("college", "batch")
 
-class CustomUserAdmin(UserAdmin):
-    list_display = ("username", "email", "first_name", "last_name", "is_staff", "type")
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (
-            "Personal Info",
-            {"fields": ("email", "first_name", "last_name", "type", "otp")},
-        ),
-        (
-            "Permissions",
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
+    def display_image(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="50px" height="50px" />'.format(obj.image.url)
+            )
+        return None
+
+    display_image.short_description = "Profile Picture"
+
+    def website_link(self, obj):
+        if obj.website_url:
+            return format_html(
+                '<a href="{}" target="_blank">{}</a>'.format(
+                    obj.website_url, obj.website_url
                 )
-            },
-        ),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
-    )
-    add_fieldsets = (
+            )
+        return None
+
+    website_link.short_description = "Website"
+
+    def linkedin_link(self, obj):
+        if obj.linkedin_url:
+            return format_html(
+                '<a href="{}" target="_blank">{}</a>'.format(
+                    obj.linkedin_url, obj.linkedin_url
+                )
+            )
+        return None
+
+    linkedin_link.short_description = "LinkedIn"
+
+
+class BlackListedTokenAdmin(admin.ModelAdmin):
+    list_display = ("token",)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "type", "otp")
+    search_fields = ("username", "email")
+    list_filter = ("type",)
+    fieldsets = (
         (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "username",
-                    "email",
-                    "password1",
-                    "password2",
-                    "is_staff",
-                    "is_active",
-                ),
-            },
+            "Personal Information",
+            {"fields": ("username", "email", ("first_name", "last_name"), "password")},
         ),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "type")}),
+        ("OTP", {"fields": ("otp",)}),
     )
-    search_fields = ("username", "email", "first_name", "last_name")
-    ordering = ("username",)
 
 
-admin.site.register(User, CustomUserAdmin)
-admin.site.register(BlackListedToken)
-admin.site.register(Profile)
+admin.site.register(User, UserAdmin)
+admin.site.register(BlackListedToken, BlackListedTokenAdmin)
+admin.site.register(Profile, ProfileAdmin)
